852e86a90a46fd68fa0241dde29183b8ae864c73
キラ☆きらら <kirakirara966@gmail.com>
2022-01-03 00:17:24 +0100

    Refactor + made tracks go the right way

--- scripts/main.js
+++ scripts/main.js
@@ -3,22 +3,24 @@ const TRACKS_NUMBER = 4;
 const TRACK_TILE_WIDTH = 64;
 const TRACK_TILE_HEIGHT = 64;
 const TRACK_HEIGHT = 8;
-const CANVAS = document.getElementById('canvas');
-const CTX = CANVAS.getContext('2d', { alpha: false });
-const TRACKS_X = (CANVAS.width - (TRACK_TILE_WIDTH * TRACKS_NUMBER)) / 2;
-const TRACKS_Y = (CANVAS.height - (TRACK_TILE_HEIGHT * TRACK_HEIGHT)) / 2;
-const START = new Date().getTime();
+
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d', { alpha: false });
+const start = new Date().getTime();
+
+const tracksX = (canvas.width - (TRACK_TILE_WIDTH * TRACKS_NUMBER)) / 2;
+const tracksY = (canvas.height - (TRACK_TILE_HEIGHT * TRACK_HEIGHT)) / 2;
 
 setInterval(() => {
-  const now = new Date();
-  const interval = now.getTime() - START;
+  const now = new Date().getTime();
+  const interval = now - start;
   drawBackground();
   drawTracks(interval);
 }, 1000 / FPS);
 
 function drawBackground() {
-  CTX.fillStyle = '#78fbcf';
-  CTX.fillRect(0, 0, CANVAS.width, CANVAS.height);
+  ctx.fillStyle = '#78fbcf';
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 }
 
 /**
@@ -26,18 +28,18 @@ function drawBackground() {
  */
 function drawTracks(interval) {
   const colors = ['#7f7f10', '#4f4f10'];
-  const offset = (interval % 1000) / 1000 * TRACK_TILE_HEIGHT;
+  const offset = TRACK_TILE_HEIGHT - ((interval % 1000) / 1000 * TRACK_TILE_HEIGHT);
   const startingColorIndex = Math.floor((interval % 2000) / 1000);
 
   const firstColor = colors[(startingColorIndex + 1) % colors.length];
-  CTX.fillStyle = firstColor;
-  CTX.fillRect(TRACKS_X, TRACKS_Y, TRACK_TILE_WIDTH * TRACKS_NUMBER, offset);
+  ctx.fillStyle = firstColor;
+  ctx.fillRect(tracksX, tracksY, TRACK_TILE_WIDTH * TRACKS_NUMBER, offset);
   for (let i = 0; i < TRACK_HEIGHT - 1; i++) {
     const color = colors[(startingColorIndex + i) % colors.length];
-    CTX.fillStyle = color;
-    CTX.fillRect(TRACKS_X, TRACKS_Y + offset + i * TRACK_TILE_HEIGHT, TRACK_TILE_WIDTH * TRACKS_NUMBER, TRACK_TILE_HEIGHT);
+    ctx.fillStyle = color;
+    ctx.fillRect(tracksX, tracksY + offset + i * TRACK_TILE_HEIGHT, TRACK_TILE_WIDTH * TRACKS_NUMBER, TRACK_TILE_HEIGHT);
   }
   const lastColor = colors[(startingColorIndex + 1) % colors.length];
-  CTX.fillStyle = lastColor;
-  CTX.fillRect(TRACKS_X, TRACKS_Y + offset + (TRACK_HEIGHT - 1) * TRACK_TILE_HEIGHT, TRACK_TILE_WIDTH * TRACKS_NUMBER, TRACK_TILE_HEIGHT - offset);
+  ctx.fillStyle = lastColor;
+  ctx.fillRect(tracksX, tracksY + offset + (TRACK_HEIGHT - 1) * TRACK_TILE_HEIGHT, TRACK_TILE_WIDTH * TRACKS_NUMBER, TRACK_TILE_HEIGHT - offset);
 }
