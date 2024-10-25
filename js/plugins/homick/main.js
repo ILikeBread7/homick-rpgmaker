29a4baf5754a2f0c0ef4397229b7ea3f8dba5f0c
ILikeBread7 <pjaworski47@gmail.com>
2024-10-25 18:00:26 +0200

    Homick per level speeds

--- js/plugins/homick/main.js
+++ js/plugins/homick/main.js
@@ -13,15 +13,7 @@ class HomickRacer {
       : Obstacles.createObstaclesForLevel(level, totalDistance)
     ;
 
-    const homicks = level === -1
-      ? [ { acceleration: 0.75, maxSpeed: 2, player: () => new HumanPlayer() } ]
-      : [
-        { acceleration: 0.5, maxSpeed: 3, player: () => new HumanPlayer() },
-        { acceleration: 2, maxSpeed: 2.5, player: (homick, obstacles) => new SimpleAi(homick, obstacles, 20, 10) },
-        { acceleration: 2, maxSpeed: 2.5, player: (homick, obstacles) => new SimpleAi(homick, obstacles, 10, 8) },
-        { acceleration: 2, maxSpeed: 2.72, player: (homick, obstacles) => new SimpleAi(homick, obstacles, 12, 10, 6, 3) }
-      ];
-    
+    const homicks = this._getHomicksForLevel(level);
     const race = new Race(window, bgmName, homicks, obstacles, totalDistance);
     return race;
   }
@@ -49,7 +41,7 @@ class HomickRacer {
     const homicks = [];
     
     for (let i = 1; i <= numberOfHumanPlayers; i++) {
-      homicks.push({ acceleration: 0.5, maxSpeed: 3, player: () => new HumanPlayer(i) });
+      homicks.push({ acceleration: 0.75, maxSpeed: 3, player: () => new HumanPlayer(i) });
     }
 
     if (numberOfCpuPlayers) {
@@ -88,5 +80,51 @@ class HomickRacer {
     const minDistance = TRACKS_HEIGHT * 5;
     const levelIndex = level - 1;
     return minDistance + Math.floor(levelIndex * distancePerStage / levelsPerStage) + (levelIndex % levelsPerStage) * distancePerLevel;
+  }
+
+  /**
+   * 
+   * @param {number} level 
+   * @returns 
+   */
+  static _getHomicksForLevel(level) {
+    // Endless mode
+    if (level === -1) {
+      return [ { acceleration: 0.75, maxSpeed: 2.5, player: () => new HumanPlayer() } ];
+    }
+
+    const playerMinSpeed = 2.5;
+    const playerMaxSpeed = 3.5;
+    const playerSpeed = this._getHomickSpeedForLevel(level, playerMinSpeed, playerMaxSpeed);
+    
+    const homicks = [ { acceleration: 0.75, maxSpeed: playerSpeed, player: () => new HumanPlayer() } ];
+
+    // Not a boss level, add mobs
+    if (level % 4 !== 0) {
+      const homickMobMinSpeed = 2;
+      const homickMobMaxSpeed = 3.15;
+      const homickMobSpeed = this._getHomickSpeedForLevel(level, homickMobMinSpeed, homickMobMaxSpeed);
+      homicks.push(
+        { acceleration: 2, maxSpeed: homickMobSpeed, player: (homick, obstacles) => new SimpleAi(homick, obstacles, 20, 10, 8, 5) },
+        { acceleration: 2, maxSpeed: homickMobSpeed, player: (homick, obstacles) => new SimpleAi(homick, obstacles, 10, 8,  5, 4) }
+      );
+    }
+
+    // Boss homick
+    homicks.push({ acceleration: 2, maxSpeed: playerSpeed, player: (homick, obstacles) => new SimpleAi(homick, obstacles, 15, 7, 6, 3) });
+
+    return homicks;
+  }
+
+  /**
+   * 
+   * @param {number} level 
+   * @param {number} minSpeed 
+   * @param {number} maxSpeed 
+   * @returns 
+   */
+  static _getHomickSpeedForLevel(level, minSpeed, maxSpeed) {
+    const maxLevel = 16;
+    return minSpeed + (level / maxLevel) * (maxSpeed - minSpeed);
   }
 }
