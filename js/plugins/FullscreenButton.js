6e2047f8ac70380cb7923a3778b762aafe608444
ILikeBread7 <pjaworski47@gmail.com>
2024-10-30 17:00:00 +0100

    Fullscreen button plugin

---
+++ js/plugins/FullscreenButton.js
@@ -0,0 +1,75 @@
+//=============================================================================
+// FullscreenButton.js
+//=============================================================================
+
+/*:
+ * @plugindesc Adds a fullscreen button to the bottom right of the screen.
+ * @author ChatGPT with my (I_LIKE_BREAD7) fixes
+ *
+ * @param Disable expression
+ * @desc Eval expression to disable the plugin, the default disables for mobile app, to enable everywhere use false
+ * @default window.cordova || window.navigator.standalone
+ * 
+ * @param Button text
+ * @desc Text for the button
+ * @default Fullscreen
+ * 
+ * @help This plugin adds a button to the bottom right of the screen
+ * that allows players to toggle fullscreen mode in RPG Maker MV.
+ */
+
+(function() {
+
+    const parameters = PluginManager.parameters('FullscreenButton');
+    const buttonText = parameters['Button text'] || '';
+    const disableExpression = parameters['Disable expression'] || '';
+
+    if (eval(disableExpression)) {
+        return;
+    }
+
+    const normalBgColor = '#3689be';
+    const hoverBgColor = '#0d517c';
+    const buttonStyles = `
+        position: absolute;
+        bottom: 3px;
+        right: 3px;
+        padding: 4px;
+        font-size: 12px;
+        background-color: ${normalBgColor};
+        color: #fff;
+        border-style: solid;
+        border-width: 1px;
+        border-radius: 5px;
+        border-color: #d6d6d6;
+        cursor: pointer;
+        z-index: 1000;
+    `;
+
+    const createFullscreenButton = () => {
+        const button = document.createElement('button');
+        button.innerHTML = buttonText;
+        button.style.cssText = buttonStyles;
+        
+        button.onclick = () => {
+            Graphics._switchFullScreen();
+        };
+
+        button.onmouseover = () => {
+            button.style.backgroundColor = hoverBgColor;
+        };
+
+        button.onmouseleave = () => {
+            button.style.backgroundColor = normalBgColor;
+        };
+
+        document.body.appendChild(button);
+    };
+
+    // Wait until the game screen is ready, then add the button
+    const _SceneManager_run = SceneManager.run;
+    SceneManager.run = function(sceneClass) {
+        _SceneManager_run.call(this, sceneClass);
+        createFullscreenButton();
+    };
+})();
