39b13a5f093eb0ca833ea6d5dc00d0674fe94a64
ILikeBread7 <pjaworski47@gmail.com>
2024-12-06 18:55:33 +0100

    Removed console.log

--- js/plugins/homick/race.js
+++ js/plugins/homick/race.js
@@ -499,7 +499,6 @@ class Race {
   }
 
   _drawPauseButton() {
-    console.log(TouchInput.date)
     const pauseSpriteSize = 48;
     const tileIndex = (TouchInput.x >= BASE_WIDTH - pauseSpriteSize && TouchInput.y <= pauseSpriteSize)
       ? PAUSE_HOVERED_TILE_INDEX
